e04ae879efad12583f5e1d4112206a8d035c4b64
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-05-31 14:53:46 +0200

    JobID añadido

--- metilapp/views.py
+++ metilapp/views.py
@@ -10,11 +10,14 @@ def main_page(request):
         form = MetilForm(request.POST, request.FILES)
         if form.is_valid():
             f=request.FILES['file']
+            jobID = f.name
+            if form.data['job_ID']:
+                jobID = form.data['job_ID']
             handle_uploaded_file(f)
             data_gff = read_file_gff('metilapp/'+f.name)
             base_sts = methyl_type_stadistics(data_gff)
             delete_file(f)
-            return render(request,'metilapp/result.html', {'message': message, 'total_m': base_sts[0], 'chrom_m': base_sts[1], 'name_gff': f.name})
+            return render(request,'metilapp/result.html', {'message': message, 'total_m': base_sts[0], 'chrom_m': base_sts[1], 'jobID': jobID})
         else:
             message = form.errors
     form = MetilForm()
