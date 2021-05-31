12ebc7a1e6a05fc6fd88b707a8567aea44f2057c
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-06-01 00:13:49 +0200

    Form con patrones

--- metilapp/validators.py
+++ metilapp/validators.py
@@ -1,7 +1,56 @@
+import os
+from django.core.exceptions import ValidationError
+
 def validate_file_extension(value):
-    import os
-    from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.gff']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
+
+def validate_pos_pat(value):
+    if value <= 0:
+        raise ValidationError('Incorrect position.')
+
+def validate_pos_compl(value):
+    if value < 0:
+        raise ValidationError('Incorrect value for position.')
+
+def validate_pat_IUPAC(value):
+    aux = "ACGTURYSWKMBDHVN"
+    for i in value:
+        if not i in aux:
+            raise ValidationError('No IUPAC values in pattern.')
+
+def validate_pos_in_pattern(form):
+    i = 1
+    while i <= 6:
+        aux = 'pos_pat'+str(i)
+        pat = 'patron'+str(i)
+        if form.data[aux]:
+            if len(form.data[pat])<int(form.data[aux]):
+                message= message+'\nIncorrect position for pattern '+str(i)+'.'
+        i=i+1
+    return message
+
+def validate_pos_in_compl_pat(form):
+    message=""
+    i = 1
+    while i <= 6:
+        aux = 'pos_compl_pat'+str(i)
+        pat = 'compl_pat'+str(i)
+        if form.data[aux]:
+            if len(form.data[pat])<int(form.data[aux]):
+                message= message+'\nIncorrect position for complementary pattern '+str(i)+'.'
+        i=i+1
+    return message
+
+def validate_comp_if_pattern(form):
+    message=""
+    i = 1
+    while i <= 6:
+        aux = 'compl_pat'+str(i)
+        pat = 'patron'+str(i)
+        if form.data[aux] and not form.data[pat]: 
+            message= message+'\nInvalid lone complementary pattern '+str(i)+'.'
+        i=i+1
+    return message
--- metilapp/views.py
+++ metilapp/views.py
@@ -1,3 +1,4 @@
+from .validators import validate_comp_if_pattern, validate_pos_in_compl_pat, validate_pos_in_pattern
 from .forms import MetilForm
 from .functions import delete_file, handle_uploaded_file, methyl_type_stadistics, read_file_gff
 from django.shortcuts import render
@@ -5,11 +6,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
+    validation=""
     message=""
+
     if request.method=='POST':
         form = MetilForm(request.POST, request.FILES)
-        if form.is_valid():
-            f=request.FILES['file']
+        validation=validate_pos_in_pattern(form) + validate_pos_in_compl_pat(form) + validate_comp_if_pattern(form)
+        if form.is_valid() and validation=="":
+            f=request.FILES['m_out']
             jobID = f.name
             if form.data['job_ID']:
                 jobID = form.data['job_ID']
@@ -21,4 +25,4 @@ def main_page(request):
         else:
             message = form.errors
     form = MetilForm()
-    return render(request,'metilapp/main_page.html', {'form': form,'message': message})
+    return render(request,'metilapp/main_page.html', {'form': form,'message': message, 'val': validation})
