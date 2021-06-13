87b2f1d3866d6eefa22516ba84cb7ef68a92fd59
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-06-13 14:29:23 +0200

    Metilaciones con gen asociado y correciiones

--- metilapp/views.py
+++ metilapp/views.py
@@ -1,6 +1,6 @@
 from .validators import validate_comp_if_pattern, validate_pos_in_compl_pat, validate_pos_in_pattern
 from .forms import MetilForm
-from .functions import delete_fasta, delete_file, handle_uploaded_file, input_builder, methyl_type_stadistics, patterns_in_genome, read_fasta, read_file_gff, read_gene_gff, met_in_genes
+from .functions import delete_file, handle_uploaded_file, input_builder, methyl_type_stadistics, read_fasta, read_file_gff, read_gene_gff, met_in_genes
 from django.shortcuts import render
 
 def main_page(request):
@@ -38,7 +38,7 @@ def main_page(request):
 
             return render(request,'metilapp/result.html', {'message': message, 'patterns': patterns.items(), 'met_name': f.name, 'fasta_name': g.name, 'gene_name': gene.name,
              'prom': form.data['prom'], 'jobID': jobID, 'total_m': base_sts[0], 'chrom_m': base_sts[1], 'index_pat': data_fasta[0].items(), 'pat_status': data_fasta[1][0], 
-                'num_st': data_fasta[1][1], 'metGen': data_metGen[0], 'metProm': data_metGen[1]})
+                'num_st': data_fasta[1][1], 'metGen': data_metGen[0], 'metProm': data_metGen[1], 'genMets': data_metGen[2], 'promMets': data_metGen[3]})
         else:
             message = form.errors
     form = MetilForm()
