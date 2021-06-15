5dbeaadf2776c53a9e213a79e2871f1982e589fb
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-06-16 00:52:33 +0200

    Promotores no mostrados error y debug Heroku

--- metilapp/views.py
+++ metilapp/views.py
@@ -39,7 +39,7 @@ def main_page(request):
             return render(request,'metilapp/result.html', {'message': message, 'patterns': patterns.items(), 'met_name': f.name, 'fasta_name': g.name, 'gene_name': gene.name,
              'prom': form.data['prom'], 'jobID': jobID, 'total_m': base_sts[0], 'chrom_m': base_sts[1], 'index_pat': data_fasta[0].items(), 'pat_status': data_fasta[1][0], 
                 'num_st': data_fasta[1][1], 'metGen': data_metGen[0], 'metProm': data_metGen[1], 'genMets': data_metGen[2].items(), 'promMets': data_metGen[3].items(),
-                'pattGen': data_fasta[2], 'pattProm': data_fasta[2]})
+                'pattGen': data_fasta[2], 'pattProm': data_fasta[3]})
         else:
             message = form.errors
     form = MetilForm()
