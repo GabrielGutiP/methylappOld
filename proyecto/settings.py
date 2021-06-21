a728e8399e554fcb2719901ec4dbc443f038d16d
gabgutpri@alum.us.es <gabgutpri@alum.us.es>
2021-06-22 01:42:15 +0200

    Pruebas heroku

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b=y$+qq*u0ewsjm2*rhm=oae^vw9=+9)&shtav0v1ius=k0l&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
